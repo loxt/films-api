c234c41f36eaebae157408d50b0a79c01121ef82
loxt <emannuelloxt@hotmail.com>
2022-11-04 23:28:35 -0400

    feat: add swagger

--- src/films/films.controller.ts
+++ src/films/films.controller.ts
@@ -1,10 +1,29 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { FilmsService } from './films.service';
+import { ApiImplicitQuery } from '@nestjs/swagger/dist/decorators/api-implicit-query.decorator';
 
 @Controller('films')
 export class FilmsController {
   constructor(private readonly filmsService: FilmsService) {}
 
+  @ApiImplicitQuery({
+    name: 'limit',
+    description: 'The maximum number of films to return',
+    required: false,
+    type: Number,
+  })
+  @ApiImplicitQuery({
+    name: 'offset',
+    description: 'The number of films to skip',
+    required: false,
+    type: Number,
+  })
+  @ApiImplicitQuery({
+    name: 'fields',
+    description: 'The fields to return',
+    required: false,
+    type: String,
+  })
   @Get()
   // get query params
   async findAll(
@@ -18,6 +37,12 @@ export class FilmsController {
     return this.filmsService.findAll(limit, offset, fieldsArray);
   }
 
+  @ApiImplicitQuery({
+    name: 'fields',
+    description: 'The fields to return',
+    required: false,
+    type: String,
+  })
   @Get(':id')
   findOne(@Param('id') id: string, @Query('fields') fields: string) {
     const fieldsArray = fields ? fields.split(',') : [];
